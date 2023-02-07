aa742cf7991fc1716d8a1f871906e7043f6c520f
mason <conficker77@gmail.com>
2023-02-08 01:16:10 +0900

    mod, RepositoryRestResource 추가

--- src/main/java/com/example/springbootboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/example/springbootboard/repository/ArticleCommentRepository.java
@@ -3,6 +3,8 @@ package com.example.springbootboard.repository;
 import com.example.springbootboard.domain.Article;
 import com.example.springbootboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
